c17517558d341f312094e7f2fd880f681bea7bdb
Martín Figueroa Padilla <martnfigueroa21@gmail.com>
2019-02-01 15:14:56 -0600

    Initial Commit

---
+++ SolarSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SolarSystem
+//
+//  Created by Martín Figueroa Padilla on 2/1/19.
+//  Copyright © 2019 Martín Figueroa Padilla. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
